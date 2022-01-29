a34532a2ecc08f4bed7c57f55c41a23e30d36cca
OJ <ojaschaturvedi2006@gmail.com>
2022-01-28 21:41:25 -0700

    Jump function for dino

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+}
